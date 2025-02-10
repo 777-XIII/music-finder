d84b29da98c30f38f8f7fa5e582c80db27752ec8
Leon <l.cohen@web.de>
2025-02-10 21:25:30 +0100

    edited navbar / added styledlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -13,5 +13,37 @@ const Navbar = () => {
     </nav>
   );
 };
+
+export default Navbar;
+ */
+
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background: #333;
+  padding: 10px;
+  display: flex;
+  gap: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+function Navbar() {
+  return (
+    <Nav>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/search">Search</StyledLink>
+      <StyledLink to="/favorites">Favorites</StyledLink>
+    </Nav>
+  );
+}
 
 export default Navbar;
