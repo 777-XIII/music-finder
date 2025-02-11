3ef2ea733ab8ab14d8ce757a998cf5534cd9e90b
Leon <l.cohen@web.de>
2025-02-11 15:07:07 +0100

    cleaned up and structured code in multiple files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,5 @@
-/* import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-const Navbar = () => {
-  return (
-    <div className="navbar">
-      <input type="text" placeholder="Search..." />
-      <button type="button" onClick={() => console.log("Search clicked")}>Search</button>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/charts">Charts</Link>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
- */
-
-// import React from "react";
 import { Link } from "react-router-dom";
-// import styled from "styled-components";
 
-/* const Nav = styled.nav`
-  background: #333;
-  padding: 10px;
-  display: flex;
-  gap: 20px;
-`;
-
-const StyledLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
- */
 function Navbar() {
   return (
     <nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,3 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import Favorites from './pages/Favorites';
-import { FavoritesProvider } from './context/FavoritesContext';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <FavoritesProvider>
-      <Router>
-        <App />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </Router>
-    </FavoritesProvider>
-  </React.StrictMode>
-);
- */
-
-/* sevil talk to me lol */
-
 import React from "react";
 import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
