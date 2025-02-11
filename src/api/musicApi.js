7be73beee9ade2716445bafdd86892c3caa6e588
sevil <sevil.aktas@dci-student.org>
2025-02-11 13:33:59 +0100

    api edited

--- src/api/musicApi.js
+++ src/api/musicApi.js
@@ -1,20 +1,11 @@
-// const API_URL = "/api/user/2529"; // Use proxy path for API requests
-
-// export const fetchSongs = async () => {
-//   try {
-//     const response = await fetch(API_URL);
-//     console.log("API Response:", response); // Log the response for debugging
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const data = await response.json();
-//     console.log("Fetched songs data:", data); // Log the fetched data for debugging
-//     return data.data; // Assuming data.data contains the songs
-//   } catch (error) {
-//     console.error("Error fetching songs:", error);
-//     throw error;
-//   }
-// };
+export const searchMusic = async (query) => {
+    try {
+      const response = await fetch(`https://api.deezer.com/search?q=${query}`);
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      console.error("Fehler beim Abrufen der Musik:", error);
+      return [];
+    }
+  };
+  
