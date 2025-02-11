63ade359c6657fee5a27991deceae79e4142407d
sevil <sevil.aktas@dci-student.org>
2025-02-11 15:57:46 +0100

    favorites edited

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { FavoritesContext } from "../context/FavoritesContext";
+import { useContext } from "react";
+import { SongCard } from "../components/SongCard";
 
-const Favorites = () => {
+function Favorites() {
+  const { favorites } = useContext(FavoritesContext);
   return (
     <div>
-      <h2>Favorites</h2>
-      {/* Favorites functionality will be implemented here */}
+      <h2>Deine Favoriten</h2>
+      {favorites.length === 0 ? (<p>Keine Favoriten gespeichert.</p>) 
+      : (favorites.map((song) => <SongCard key ={song.id} song={song} />)
+      )}
     </div>
   );
-};
+
+}
 
 export default Favorites;
