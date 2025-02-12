51f1eb609ef4de486b4401cf91c9b4515f800036
sevil <sevil.aktas@dci-student.org>
2025-02-12 16:14:53 +0100

    fav button edited

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,12 +1,21 @@
 import "../styles/Global.css";
 
+import React, { useContext } from "react";
+import { FavoritesContext } from "../context/FavoritesContext";
+
 function SongCard({ song }) {
+  const { toggleFavorite, favorites } = useContext(FavoritesContext);
+  const isFavorite = favorites.some((fav) => fav.id === song.id);
+
   return (
-    <div className="song-card">
+    <div>
       <img src={song.album.cover} alt={song.title} />
       <h3>
         {song.title} - {song.artist.name}
       </h3>
+      <button onClick={() => toggleFavorite(song)}>
+        {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+      </button>
       <audio controls>
         <source src={song.preview} type="audio/mpeg" />
       </audio>
