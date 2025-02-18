88393cf75eee58b7c89d2af8ba4a783de4277c5d
Leon <l.cohen@web.de>
2025-02-18 01:34:36 +0100

    extended error handling in musicApi.js

--- src/api/musicApi.js
+++ src/api/musicApi.js
@@ -3,11 +3,13 @@ export const searchMusic = async (query) => {
     const response = await fetch(
       `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${query}`
     );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
     const data = await response.json();
-
     return data.data;
   } catch (error) {
     console.error("Error trying to load music:", error);
-    return [];
+    throw new Error("Failed to fetch music. Please try again later.");
   }
 };
