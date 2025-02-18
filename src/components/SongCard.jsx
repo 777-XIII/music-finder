12ec3f1e1708bd1f72ae61c86c8a04046d5a2be4
Leon <l.cohen@web.de>
2025-02-18 04:40:15 +0100

    added doublicate prevention in songcard.jsx and style

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -10,10 +10,23 @@ function SongCard({ song }) {
   const isFavorite = favorites.some((fav) => fav.id === song.id);
   const [showPopup, setShowPopup] = useState(false);
   const [newPlaylistName, setNewPlaylistName] = useState("");
+  const [popupMessage, setPopupMessage] = useState("");
 
   const handleAddToPlaylist = (playlistId) => {
-    addSongToPlaylist(playlistId, song);
-    setShowPopup(false);
+    const playlist = playlists.find((p) => p.id === playlistId);
+    const isSongInPlaylist = playlist.songs.some((s) => s.id === song.id);
+
+    if (!isSongInPlaylist) {
+      addSongToPlaylist(playlistId, song);
+      setPopupMessage("Song added to playlist!");
+    } else {
+      setPopupMessage("This song is already in the playlist!");
+    }
+
+    setTimeout(() => {
+      setShowPopup(false);
+      setPopupMessage("");
+    }, 3000);
   };
 
   const handleCreatePlaylist = () => {
@@ -46,6 +59,7 @@ function SongCard({ song }) {
       {showPopup && (
         <div className="playlist-popup">
           <h4>Select a Playlist</h4>
+          {popupMessage && <p className="popup-message">{popupMessage}</p>}
           <ul>
             {playlists.map((playlist) => (
               <li
