057aae47342b4cf9b2b3ca9b8e3f61949f89adce
sevil <sevil.aktas@dci-student.org>
2025-02-18 01:01:02 +0100

    created homepage content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
+import Playlists from "./MyPlaylist";
+import Favorites from "./Favorites";
+import Search from "./Search";
+
+
 function Home() {
   return (
-    <div className="container">
-      <h1 className="neon-text">Welcome to MusicFinder</h1>
-      <p>Search for your favorite songs and add them to your playlist</p>
-      <p>Click on 'Search' to get started</p>
+    <div>
+      {/* Oberer Bereich mit Willkommensnachricht */}
+      <div className="container">
+        <h1 className="neon-text">Welcome to MusicFinder</h1>
+        <p>Search for your favorite songs and add them to your playlist</p>
+        <p>Click on 'Search' to get started</p>
+      </div>
+
+      {/* Unterer Bereich mit den drei Containern */}
+      <div className="home-container">
+        {/* Container für Playlists */}
+        <div className="home-box">
+          <h2>My Playlists</h2>
+          <Playlists />
+        </div>
+
+        {/* Container für Favorites */}
+        <div className="home-box">
+          <h2>Favorites</h2>
+          <Favorites />
+        </div>
+
+        {/* Container für Search */}
+        <div className="home-box">
+          <h2>Search</h2>
+          <Search />
+        </div>
+      </div>
     </div>
   );
 }
